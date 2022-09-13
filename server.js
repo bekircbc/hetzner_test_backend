f838c313cd619869ed406fa6989abe8eff3bbea5
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-13 15:12:13 +0200

    cors and express is aktiv..

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
 const port = 9384;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const employees = [
