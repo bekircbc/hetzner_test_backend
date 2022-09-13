7e2b3270585f7f29fc3534e102aae2d4690099b9
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-13 14:58:04 +0200

    cors and express.json closed...

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 const port = 9384;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
 const employees = [
   {
